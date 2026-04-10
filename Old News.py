60687b8e44ad2d06a312d9b5b6e71dce50237419
HSGRemi <remi.delafortelle@student.unisg.ch>
2026-04-10 11:54:08 +0200

    Update Old News.py

--- Old News.py
+++ Old News.py
@@ -13,7 +13,7 @@ st.caption(f"Last updated: {datetime.now().strftime('%Y-%m-%d')}")
 
 @st.cache_data(ttl=1800)
 def get_headlines():
-    feeds=["https://www.cnbc.com/id/100727362/device/rss/rss.html", "https://www.cnbc.com/world/?region=world"]
+    feeds=["https://www.cnbc.com/id/100727362/device/rss/rss.html"]
     entries=[]
     for url in feeds:
         feed=feedparser.parse(url)
